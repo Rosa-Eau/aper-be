866a9e856ae1766b7668fc595b3e435ea73830d5
Ankur <iamankursin@gmail.com>
2023-12-15 14:59:05 +0530

    added cors poilcy

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ const cors = require('cors');
 // Routes 
 const userRouter = require("./routes/users/user-route");
 const storyRouter = require("./routes/stories/story-route");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(cors())
 app.use(express.json())
 
