905ffcd03d249256b5b0bdcd0ee9772beff54bcd
Ankur <as797007@gmail.com>
2024-01-16 17:19:55 +0530

    updated main file

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const app = express();;
+const app = express();
 const bodyParser = require('body-parser');
-const path = require ('path')
+const path = require('path');
+const cors = require('cors');
+const port = 3001;
+
 app.use(bodyParser.json());
-// Parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3001;
-const cors = require('cors');
-// Routes 
-const userRouter = require("./routes/users/user-route");
-const storyRouter = require("./routes/stories/story-route");
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://aper.cc');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -18,25 +15,25 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors())
-app.use(express.json())
+// Use the cors middleware
+app.use(cors());
 
-//implementing static path
+// Implementing static path
 app.use(express.static(path.join("uploads", '../public')));
 
-require('mongoose')
-require('./db/db')
+require('mongoose');
+require('./db/db');
 
+// Routes
+const userRouter = require('./routes/users/user-route');
+const storyRouter = require('./routes/stories/story-route');
+app.use('/user', userRouter);
+app.use('/story', storyRouter);
 
-//Route Redirection
-app.use('/user',userRouter);
-app.use('/story',storyRouter);
+app.get('/', (req, res) => {
+    res.send('Server is connected');
+});
 
-app.listen(port)
-app.get('/',(req,res)=>{
-    res.send("Server is connected")
-    
-}) 
-console.log("server is running on port ", port)
-
-
+app.listen(port, () => {
+    console.log('Server is running on port ', port);
+});
