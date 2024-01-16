75caeaa95afdbb7b68f5486e5d3ccdc5211d8d96
Ankur <as797007@gmail.com>
2024-01-16 17:01:25 +0530

    added cors header https://aper.cc

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const userRouter = require("./routes/users/user-route");
 const storyRouter = require("./routes/stories/story-route");
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://aper.cc');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
