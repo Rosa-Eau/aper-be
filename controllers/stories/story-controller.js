3360756980405f9a1bb712e9fd8e3afefedae1fa
Ankur <as797007@gmail.com>
2024-03-26 12:44:22 +0530

    something

--- controllers/stories/story-controller.js
+++ controllers/stories/story-controller.js
@@ -563,13 +563,13 @@ exports.getEpisodeByAuthor = async (req, res) => {
         if (foundEpisode && foundEpisode.length > 0) {
             const episodesWithStories = await Promise.all(
                 descendingOrderEpisodes.map(async (episode) => {
-                    if(episode.storyId!=null){
+                    // if(episode.storyId!=null){
                         const story = await storyDataAccess.findStoryByStoryId(episode.storyId);
                         return {
                             ...episode.toObject(),
                                 lineStyle: story.lineStyle 
                         };
-                    }
+                    // }
                 })
             );
 
